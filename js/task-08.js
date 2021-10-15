fd84a637b584c9cd147899e78422249d1c48c2d1
Olga Saltanovskaya <saltanoffskaya@gmail.com>
2021-10-15 19:28:02 +0300

    submit the form with validation for empty fields

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormClick);
+
+function onFormClick(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Please fill in all the fields!");
+  }
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(formData);
+  event.currentTarget.reset();
+}
