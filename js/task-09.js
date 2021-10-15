df16d3b5a88347bf35471304549a43dbfcaf422c
Olga Saltanovskaya <saltanoffskaya@gmail.com>
2021-10-15 19:50:40 +0300

    change the background colors of the <body> through the inline style when clicking on the button and displays the color value

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  body: document.body,
+  btnChangeColor: document.querySelector(".change-color"),
+  spanColor: document.querySelector(".color"),
+};
+
+refs.btnChangeColor.addEventListener("click", onBtnClick);
+
+function onBtnClick() {
+  const randomColor = (refs.body.style.backgroundColor = getRandomHexColor());
+
+  refs.spanColor.textContent = randomColor;
+}
+
+console.log(refs.body);
