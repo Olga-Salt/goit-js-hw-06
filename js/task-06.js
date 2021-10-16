a58fb19c4da711c0ba6f6537e8bf82c1fad8de23
Olga Saltanovskaya <saltanoffskaya@gmail.com>
2021-10-16 11:05:55 +0300

    fix the check to be strict equal to 6

--- js/task-06.js
+++ js/task-06.js
@@ -2,30 +2,14 @@ const inputRef = document.querySelector("#validation-input");
 
 inputRef.addEventListener("blur", onInputBlur);
 
-// function onInputBlur(event) {
-//   console.log(inputRef.placeholder);
-//   if (inputRef.value.length < inputRef.dataset.length) {
-//     inputRef.classList.add("invalid");
-//   }
-
-//   inputRef.classList.remove("invalid");
-//   inputRef.classList.add("valid");
-
-//   console.dir(event.currentTarget);
-// }
-
 function onInputBlur(event) {
   const eve = event.currentTarget;
-  if (inputRef.value.length >= inputRef.dataset.length) {
+  if (inputRef.value.length === 6) {
     eve.classList.add("valid");
-  }
-
-  eve.classList.add("invalid");
-
-  if (eve.classList.contains("valid")) {
-    eve.classList.remove("invalid");
-  } else if (eve.classList.contains("invalid")) {
-    eve.classList.remove("valid");
+    eve.classList.replace("invalid", "valid");
+  } else {
+    eve.classList.add("invalid");
+    eve.classList.replace("valid", "invalid");
   }
 
   console.dir(eve.classList);
