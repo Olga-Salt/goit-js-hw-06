d31d812a67392e974d453bf6fe8420cf32eb538c
Olga Saltanovskaya <saltanoffskaya@gmail.com>
2021-10-16 11:42:22 +0300

    create and clean up item collections (boxes)
    didn't finished

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,36 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const boxControls = document.querySelector("#controls");
+
+const btnCreate = document.querySelector("[data-create]");
+const btnDestroy = document.querySelector("[data-destroy]");
+const inputNamber = btnCreate.previousElementSibling;
+
+const boxesContainer = document.querySelector("#boxes");
+
+let countOfBox = [];
+
+// inputNamber.addEventListener("input", onInputInput);
+
+// function onInputInput() {
+//   countOfBox.push(inputNamber.value);
+//   console.log(countOfBox);
+// }
+
+btnCreate.addEventListener("click", onCreatClick);
+
+function onCreatClick() {
+  createBoxes();
+}
+
+function createBoxes(amount) {
+  let murkup = `<div style =
+    'background-color: ${getRandomHexColor()};
+    height: 30px;
+    width: 30px;'
+></div>`;
+
+  boxesContainer.insertAdjacentHTML("afterbegin", murkup);
+}
