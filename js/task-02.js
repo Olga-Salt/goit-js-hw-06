25c9df9f4dddbc4017f4095b53b3e6e800edb621
Olga Saltanovskaya <saltanoffskaya@gmail.com>
2021-10-16 11:11:09 +0300

    Create a list of items and add a name to each

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const elements = [];
+
+ingredients.forEach((element) => {
+  const item = document.createElement("li");
+  item.textContent = element;
+  item.classList.add("item");
+
+  elements.push(item);
+});
+
+const listRef = document.querySelector("#ingredients");
+listRef.append(...elements);
+console.log(listRef);
